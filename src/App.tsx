6d73f4234f0a65a88d947864aaf665fb432a63cd
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-03-24 17:38:58 -0300

    desabilitando campos ao mostrar resultados

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from './App.module.css'
+import leftArrowImage from './assets/leftarrow.png'
 import poweredImage from './assets/powered.png'
 import { GridItem } from './components/GridItem'
 import { calculateImc, Level, levels } from './helpers/imc'
@@ -17,6 +18,12 @@ export default function App() {
     }
   }
 
+  const handleBackButton = () =>{
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  }
+
   return (
     <div className={styles.main}>
       <header>
@@ -30,15 +37,15 @@ export default function App() {
           <h1>Calcule o seu IMC.</h1>
           <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
 
-          <input type="number" value={heightField > 0 ? heightField : ''} onChange={(e)=>{
+          <input disabled={toShow ? true : false} type="number" value={heightField > 0 ? heightField : ''} onChange={(e)=>{
            setHeightField(parseFloat(e.target.value))
           }} placeholder="Digite a sua altura. Ex: 1.5 (em metros)" />
 
-          <input type="number" value={weightField > 0 ? weightField : ''} onChange={(e)=>{
+          <input disabled={toShow ? true : false} type="number" value={weightField > 0 ? weightField : ''} onChange={(e)=>{
            setWeightField(parseFloat(e.target.value))
           }} placeholder="Digite o seu peso. Ex: 75.3 (em kg)" />
 
-          <button onClick={handleCalculateButton}>Calcular</button>
+          <button disabled={toShow ? true : false} onClick={handleCalculateButton}>Calcular</button>
 
 
         </div>
@@ -53,7 +60,9 @@ export default function App() {
         }
         {toShow &&
           <div className={styles.rightBig}>
-            <div className={styles.rightArrow}></div>
+            <div className={styles.rightArrow} onClick={handleBackButton}>
+              <img src={leftArrowImage} alt="" width={25}/>
+            </div>
             <GridItem item={toShow}/>
           </div>
         }
