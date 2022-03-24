2b3b593378a55dc92283924c02353827352894bf
Matthew Rachid Romie Nascimento <matthewromie@hotmail.com>
2022-03-24 16:11:58 -0300

    Estilização dos inputs

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import styles from './App.module.css'
+import poweredImage from './assets/powered.png'
+
+export default function App() {
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weightField, setWeightField] = useState<number>(0)
+
+  const handleCalculateButton = () =>{
+    if(heightField && weightField){
+
+    }else{
+      alert('Digite todos os campos.');
+    }
+  }
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
+      </header>
+      <div className={styles.container}>
+        
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+          <input type="number" value={heightField > 0 ? heightField : ''} onChange={(e)=>{
+           setHeightField(parseFloat(e.target.value))
+          }} placeholder="Digite a sua altura. Ex: 1.5 (em metros)" />
+
+          <input type="number" value={weightField > 0 ? weightField : ''} onChange={(e)=>{
+           setWeightField(parseFloat(e.target.value))
+          }} placeholder="Digite o seu peso. Ex: 75.3 (em kg)" />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+
+
+        </div>
+
+        <div className={styles.rightSide}>....</div>
+
+      </div>
+    </div>
+  )
+}
